1c6d43a1324480cf22a3c8b47cfb4cdf8cd52f35
pencildev <pencil.bsp@gmail.com>
2025-03-01 18:05:20 +0700

    feat(build): implement build process for modules, types, and styles

--- scripts/utils/helper.js
+++ scripts/utils/helper.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import { outputDir } from './output-dir.js';
+
 export function capitalizeString(str) {
   return str.replace(/(?:^|-)([a-z])/g, (m, char) => char.toUpperCase());
 }
@@ -15,4 +18,44 @@ export function parseSwiperBuildModulesEnv() {
     if (buildModules.length) return buildModules;
   }
   return null;
+}
+
+function addPathPrefix(obj, need, prefix) {
+  const regex = /^\.+\//;
+
+  if (typeof obj === 'string') {
+    return regex.test(obj) ? obj.replace(/^\.+\//, prefix) : prefix + obj;
+  }
+
+  if (typeof obj === 'object') {
+    need.forEach((key) => {
+      if (obj[key]) {
+        const nextOjb = obj[key];
+        obj[key] = addPathPrefix(
+          nextOjb,
+          typeof nextOjb === 'object' ? Object.keys(nextOjb) : [],
+          prefix,
+        );
+      }
+    });
+  }
+
+  return obj;
+}
+
+export function exportProd() {
+  const pkgPath = new URL('../../package.json', import.meta.url);
+  const pkg = JSON.parse(fs.readFileSync(pkgPath));
+  const childPkg = JSON.parse(
+    fs.readFileSync(new URL(`../../${outputDir}/package.json`, import.meta.url)),
+  );
+
+  const need = ['typings', 'main', 'module', 'exports', 'typesVersions'];
+
+  const configs = addPathPrefix(childPkg, need, `./${outputDir}/`);
+  need.forEach((key) => {
+    pkg[key] = configs[key];
+  });
+
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg));
 }
